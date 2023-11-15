ea4d8649948443062ee3ec8d0f3a2b09f673ebfb
holacomoestas22 <145036535+holacomoestas22@users.noreply.github.com>
2023-11-14 18:56:45 -0600

    Create contrasena.py

---
+++ contrasena.py
@@ -0,0 +1,12 @@
+#Password-matrícula
+
+contrasena = ""
+acumulador = 0
+
+con = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o','p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x','y', 'z', 'A', 'B', 'C', 'D','E', 'F','G','H', 'I','J','K','L','M','N','Ñ','O','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9']
+while len(contrasena) <= 7:
+  print("")
+  acumulador+=1
+  lo_pon = input ("ingresa el caracter de tu contrasena: ")
+  contrasena+= lo_pon
+if lo_pon 
